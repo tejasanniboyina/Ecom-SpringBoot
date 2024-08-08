314e84bd5551fdf2976063fed9515d9b4171ac54
axlotlx <tejayadav435@gmail.com>
2024-08-08 18:51:50 +0530

    started to build

---
+++ ecomproj/src/main/java/com/teja/ecomproj/EcomprojApplication.java
@@ -0,0 +1,13 @@
+package com.teja.ecomproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomprojApplication.class, args);
+	}
+
+}
---
+++ ecomproj/src/main/java/com/teja/ecomproj/controller/ProductController.java
@@ -0,0 +1,15 @@
+package com.teja.ecomproj.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+
+    @RequestMapping("/")
+    public String greet(){
+        return "Hello World..";
+    }
+}
